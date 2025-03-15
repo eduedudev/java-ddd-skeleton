f50569a6cff205d2c97dbdf46a474fc64bca70d8
Eduardo Guastay <hi@eduedu.dev>
2025-03-14 23:53:49 -0500

    build(build): add value objects

---
+++ src/main/com/devsoftec/jaap/users/shared/domain/ValueObjects/CreatedAt.java
@@ -0,0 +1,12 @@
+package com.devsoftec.jaap.users.shared.domain.ValueObjects;
+
+public final class CreatedAt extends DateTimeValueObject {
+
+	public CreatedAt(String value) {
+		super(value);
+	}
+
+	public CreatedAt() {
+		super(null);
+	}
+}

fcd2dad64629140dc61a99f68055b70760ef25af
Eduardo Guastay <hi@eduedu.dev>
2025-03-22 12:52:05 -0500

    chore(share): add BooleanVO

--- src/main/com/jaapec/tenant/shared/domain/ValueObjects/BooleanValueObject.java
+++ src/main/com/jaapec/tenant/shared/domain/ValueObjects/BooleanValueObject.java
@@ -3,7 +3,6 @@ package com.jaapec.tenant.shared.domain.ValueObjects;
 import java.util.Objects;
 
 public abstract class BooleanValueObject {
-
 	private final Boolean value;
 
 	public BooleanValueObject(Boolean value) {
@@ -16,21 +15,21 @@ public abstract class BooleanValueObject {
 
 	@Override
 	public String toString() {
-		return "BooleanValueObject{" + "value=" + value + '}';
+		return "BoleanValueObject{" +
+			"value=" + value +
+			'}';
 	}
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 
 		BooleanValueObject that = (BooleanValueObject) o;
-
 		return Objects.equals(value, that.value);
 	}
 
 	@Override
 	public int hashCode() {
-		return value != null ? value.hashCode() : 0;
+		return Objects.hashCode(value);
 	}
 }

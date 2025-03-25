f670ea00a630461bd21750bbc5b2553c1efa659a
Eduardo Guastay <hi@eduedu.dev>
2025-03-25 01:43:41 -0500

    feat(plan): create plan ready

---
+++ src/main/com/jaapec/tenant/plans/application/create/CreatePlanCommandHandler.java
@@ -0,0 +1,35 @@
+package com.jaapec.tenant.plans.application.create;
+
+import java.math.BigDecimal;
+
+import com.jaapec.tenant.plans.domain.ValueObjects.*;
+import com.jaapec.tenant.shared.domain.Service;
+import com.jaapec.tenant.shared.domain.bus.command.CommandHandler;
+
+@Service
+public final class CreatePlanCommandHandler implements CommandHandler<CreatePlanCommand> {
+
+	private final PlanCreator creator;
+
+	public CreatePlanCommandHandler(PlanCreator creator) {
+		this.creator = creator;
+	}
+
+	@Override
+	public void handle(CreatePlanCommand command) {
+		creator.create(
+			new PlanId(command.id()),
+			new PlanName(command.name()),
+			new PlanDescription(command.description()),
+			new PlanPriceMonthly(BigDecimal.valueOf(command.priceMonthly())),
+			new PlanPriceYearly(BigDecimal.valueOf(command.priceYearly())),
+			new PlanMaxUsers(command.maxUsers()),
+			new PlanMaxRoles(command.maxRoles()),
+			new PlanMaxAccounts(command.maxAccounts()),
+			new PlanMaxInvoices(command.maxInvoices()),
+			new PlanStatus(command.status()),
+			new PlanVisibility(command.visibility()),
+			new PlanTrialDays(command.trialDays())
+		);
+	}
+}

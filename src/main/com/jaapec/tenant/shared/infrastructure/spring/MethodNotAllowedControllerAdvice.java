20a4fd2436afb99e934f5f1f535ce520fe640e45
Eduardo Guastay <eduardo.guastay@devsoftec.com>
2026-05-11 23:07:01 -0500

    feat(api): add MethodNotAllowedControllerAdvice to handle 405 errors

---
+++ src/main/com/jaapec/tenant/shared/infrastructure/spring/MethodNotAllowedControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.jaapec.tenant.shared.infrastructure.spring;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MethodNotAllowedControllerAdvice implements ErrorController {
+
+	@RequestMapping
+	public ResponseEntity<Void> handleError() {
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+	}
+}
